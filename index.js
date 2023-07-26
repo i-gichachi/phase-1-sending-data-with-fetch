36beb7c893bb1d1c89b82e638ee6ff22cf4c699f
Ian Mwaura Gichachi <iangichachi@gmail.com>
2023-07-26 09:01:11 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+    const url = "http://localhost:3000/users"
+    const data = {
+      name: name,
+      email: email
+    }
+  
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+    }
+  
+    return fetch(url, config)
+      .then(response => response.json())
+
+      .then(data => {
+        const id = data.id
+        document.body.innerHTML += `<p>${id}</p>`
+        return data
+
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`
+      })
+  }
+  
+  
